c11a5f7c6feb57d63f39d95acf2d70c5885400e5
Lubos Matl <matllubos@gmail.com>
2014-02-10 15:39:28 +0100

    Fixed Manifest file

--- block_snippets/response.py
+++ block_snippets/response.py
@@ -1,3 +1,5 @@
+import json
+
 from django.template.response import TemplateResponse
 from django.template.loader_tags import BlockNode, ExtendsNode
 
@@ -11,51 +13,76 @@ class BlockNotFound(Exception):
 class SnippetsTemplateResponse(TemplateResponse):
 
     def __init__(self, request, template, context=None, content_type=None,
-            status=None, mimetype=None, current_app=None, block_name=None):
+            status=None, mimetype=None, current_app=None, block_names=None):
         super(SnippetsTemplateResponse, self).__init__(request, template, context, content_type, status, mimetype,
                                                        current_app)
-        self.block_name = block_name
+        self.block_names = block_names
 
-
-    def _get_node(self, nodelist, context):
+    def _get_node(self, nodelist, context, block_name):
         for node in nodelist:
-            if isinstance(node, BlockNode) and node.name == self.block_name:
+            if isinstance(node, BlockNode) and node.name == block_name:
                 return node
             for key in ('nodelist', 'nodelist_true', 'nodelist_false'):
                 if hasattr(node, key):
                     try:
-                        return self._get_node(getattr(node, key), context)
+                        return self._get_node(getattr(node, key), context, block_name)
                     except:
                         pass
             if isinstance(node, SnippetsIncludeNode):
                 t = node.get_nodelist(context)
                 t._render(context)
-                subnode = self._get_node(t, context)
+                subnode = self._get_node(t, context, block_name)
                 if subnode:
                     return subnode
 
         for node in nodelist:
             if isinstance(node, ExtendsNode):
-                subnode = self._get_node(node.get_parent(context), context)
+                subnode = self._get_node(node.get_parent(context), context, block_name)
                 if subnode:
                     return subnode
 
         raise BlockNotFound
+
+    def render_block(self, template, context, block_name):
+        try:
+            template._render(context)
+            block_template = self._get_node(template, context, block_name)
+            template = block_template
+            return block_template.render(context)
+        except BlockNotFound:
+            return None
 
     @property
     def rendered_content(self):
         template = self.resolve_template(self.template_name)
         context = self.resolve_context(self.context_data)
 
-        block_template = template
+        if not self.block_names:
+            return template.render(context)
 
-        try:
-            if self.block_name:
-                template._render(context)
-                block_template = self._get_node(template, context)
-                template = block_template
-        except BlockNotFound:
-            pass
+        template._render(context)
+        return self.render_block(template, context, self.block_names[0])
 
-        content = block_template.render(context)
-        return content
+
+class JsonSnippetsTemplateResponse(SnippetsTemplateResponse):
+
+    def __init__(self, request, template, context=None, content_type=None,
+            status=None, mimetype=None, current_app=None, block_names=None):
+        content_type = content_type or block_names and 'application/json' or None
+        super(JsonSnippetsTemplateResponse, self).__init__(request, template, context, content_type, status, mimetype,
+                                                           current_app, block_names)
+
+    @property
+    def rendered_content(self):
+        template = self.resolve_template(self.template_name)
+        context = self.resolve_context(self.context_data)
+
+        if not self.block_names:
+            return template.render(context)
+
+        template._render(context)
+
+        output = {}
+        for block_name in self.block_names:
+            output[block_name] = self.render_block(template, context, block_name)
+        return json.dumps(output)
--- block_snippets/views.py
+++ block_snippets/views.py
@@ -1,16 +1,24 @@
-from block_snippets.response import SnippetsTemplateResponse
+from block_snippets.response import SnippetsTemplateResponse, JsonSnippetsTemplateResponse
 
 
 class SnippetTemplateResponseMixin(object):
     response_class = SnippetsTemplateResponse
     allowed_snippets = ()
+    allow_all_snippets = False
 
-    def get_snippet_name(self):
-        snippet = self.request.GET.get('snippet')
-        if snippet and snippet not in self.allowed_snippets:
-            snippet = None
-        return snippet
+    def get_snippet_names(self):
+        snippets = []
+
+        for snippet in self.request.GET.getlist('snippet'):
+            if self.allow_all_snippets or snippet in self.allowed_snippets:
+                snippets.append(snippet)
+
+        return snippets
 
     def render_to_response(self, context, **response_kwargs):
-        return super(SnippetTemplateResponseMixin, self).render_to_response(context, block_name=self.get_snippet_name(),
+        return super(SnippetTemplateResponseMixin, self).render_to_response(context, block_names=self.get_snippet_names(),
                                                                             **response_kwargs)
+
+
+class JsonSnippetTemplateResponseMixin(SnippetTemplateResponseMixin):
+    response_class = JsonSnippetsTemplateResponse
