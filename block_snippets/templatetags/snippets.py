52198068f2c7f740b644eaabb6cd785b5df4053d
Lukas Rychtecky <lukas.rychtecky@gmail.com>
2014-03-16 20:14:09 +0100

    Snippet generates a class name #1

--- block_snippets/templatetags/snippets.py
+++ block_snippets/templatetags/snippets.py
@@ -17,7 +17,8 @@ class SnippetNode(Node):
         return self.name.resolve(context)
 
     def render(self, context):
-        attrs = {'data-snippet': self.get_name(context), 'data-snippet-type': self.method.resolve(context)}
+        class_name = 'snippet snippet-' + self.get_name(context).split('-')[-1]
+        attrs = {'data-snippet': self.get_name(context), 'data-snippet-type': self.method.resolve(context), 'class': class_name}
         if self.web_link:
             attrs['data-web-link'] = self.web_link.resolve(context)
 
