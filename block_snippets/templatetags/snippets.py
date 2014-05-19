564d9ca55ab4279062f5cf5535d98413a649a7b9
Lubos Matl <matllubos@gmail.com>
2014-05-19 19:09:03 +0200

    v0.0.9 Change snippet loader to block tag

--- block_snippets/templatetags/snippets.py
+++ block_snippets/templatetags/snippets.py
@@ -51,15 +51,44 @@ def do_snippet(parser, token):
     return SnippetNode(snippet_name, snippet_method, snippet_web_link, nodelist)
 
 
-@register.simple_tag
-def snippet_loader(loader, type='replace', web_link=None, onload=None):
-    attrs = {
-             'data-snippet-loader': loader,
-             'data-snippet-type': type
-             }
-    if web_link:
-        attrs['data-web-link'] = web_link
-    if onload:
-        attrs['data-snippet-onload'] = onload
+class SnippetLoaderNode(Node):
 
-    return '<div class="snippet snippet-content"%s></div>' % flatatt(attrs)
+    def __init__(self, nodelist, loader, type, web_link=None, onload=None):
+        self.nodelist = nodelist
+        self.loader = loader
+        self.type = type
+        self.web_link = web_link
+        self.onload = onload
+
+    def render_content(self, context):
+        return self.nodelist.render(context)
+
+    def render(self, context):
+        attrs = {
+                 'data-snippet-loader': self.loader.resolve(context),
+                 'data-snippet-type': self.type.resolve(context),
+                 }
+        if self.web_link:
+            attrs['data-web-link'] = self.web_link.resolve(context)
+        if self.onload:
+            attrs['data-snippet-onload'] = self.onload.resolve(context)
+        if self.web_link:
+            attrs['data-web-link'] = self.web_link.resolve(context)
+        return '<div class="snippet snippet-content"%s>%s</div>' % (flatatt(attrs), self.render_content(context))
+
+@register.tag('snippet_loader')
+def do_snippet_loader(parser, token):
+    tag_args = []
+    tag_kwargs = {'type': parser.compile_filter('"replace"')}
+
+    for content in token.split_contents()[1:]:
+        if '=' in content:
+            tag_kwargs[content.split('=', 1)[0]] = parser.compile_filter(content.split('=', 1)[1])
+        else:
+            tag_args.append(parser.compile_filter(content))
+
+    if not tag_args:
+        raise TemplateSyntaxError("'snippet_loader' tag must have selected some loaders")
+    nodelist = parser.parse(('endsnippet_loader',))
+    parser.delete_first_token()
+    return SnippetLoaderNode(nodelist, *tag_args, **tag_kwargs)
