f9adf0cdd31339ace811506abf8f5b4a3e8e8dc1
Lubos Matl <matllubos@gmail.com>
2015-10-18 21:58:02 +0200

    v0.1.1 Fixed rendering for django 1.8

--- block_snippets/response.py
+++ block_snippets/response.py
@@ -3,13 +3,9 @@ from __future__ import unicode_literals
 import json
 
 from django.template.response import TemplateResponse
-from django.template.context import _current_app_undefined
+from django.template.context import make_context, _current_app_undefined
 
 from block_snippets.utils import clean_html
-
-
-class SnippetNotFound(Exception):
-    pass
 
 
 class SnippetsTemplateResponse(TemplateResponse):
@@ -21,11 +17,8 @@ class SnippetsTemplateResponse(TemplateResponse):
         self.snippet_names = snippet_names
 
     def render_snippet(self, template, context, snippet_name):
-        try:
-            snippet_template = context.render_context.get('snippets', {}).get(snippet_name)
-            return snippet_template.render_content(context)
-        except SnippetNotFound:
-            return None
+        snippet_template = context.render_context.get('snippets', {}).get(snippet_name)
+        return snippet_template.render_content(context) if snippet_template else None
 
     @property
     def rendered_content(self):
@@ -35,8 +28,12 @@ class SnippetsTemplateResponse(TemplateResponse):
         if not self.snippet_names:
             return template.render(context, self._request)
 
-        template._render(context, self._request)
-        return self.render_snippet(template, context, self.snippet_names[0])
+        context = make_context(context, self._request)
+        with context.bind_template(template.template):
+            template.template._render(context)
+            return self.render_snippet(template, context, self.snippet_names[0])
+
+        return template.render(context, self._request)
 
 
 class JsonSnippetsTemplateResponse(SnippetsTemplateResponse):
@@ -44,6 +41,7 @@ class JsonSnippetsTemplateResponse(SnippetsTemplateResponse):
     def __init__(self, request, template, context=None, content_type=None, status=None,
                  current_app=_current_app_undefined, charset=None, using=None, snippet_names=None,
                  extra_snippets=None, extra_content=None, force_snippets=False):
+
         extra_snippets = extra_snippets or {}
         extra_content = extra_content or {}
         content_type = content_type or (snippet_names or force_snippets) and 'text/plain' or None
@@ -60,23 +58,26 @@ class JsonSnippetsTemplateResponse(SnippetsTemplateResponse):
         context = self._resolve_context(self.context_data)
 
         if not self.snippet_names and not self.force_snippets:
-            return template.render(context, self._request)
+            content = template.render(context, self._request)
+            return content
 
-        template._render(context)
+        context = make_context(context, self._request)
+        with context.bind_template(template.template):
+            template.template._render(context)
+            snippets = {}
 
-        snippets = {}
+            for snippet_name in self.snippet_names:
+                snippets[snippet_name] = self.render_snippet(template, context, snippet_name) or ''
 
-        for snippet_name in self.snippet_names:
-            snippets[snippet_name] = self.render_snippet(template, context, snippet_name) or ''
         snippets.update(self.extra_snippets)
 
         for key, val in snippets.items():
             snippets[key] = clean_html(val)
 
         output = {
-                    'snippets': snippets,
-                 }
+            'snippets': snippets,
+        }
 
         output.update(self.extra_content)
-
-        return json.dumps(output)
+        content = json.dumps(output)
+        return content
